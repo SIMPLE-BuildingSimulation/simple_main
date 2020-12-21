6039beb2c1ba55668fb403b351f9ebe2101af05e
German Molina <germolinal@gmail.com>
2020-12-22 06:41:18 +1300

    some

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use building_model::building_state::BuildingState;
+use simulation_state::simulation_state::SimulationState;
 use building_model::building::Building;
-use communication_traits::simulation_model::SimulationModel;
+use communication_protocols::simulation_model::SimulationModel;
+use controller::Controller;
 
 use calendar::date_factory::DateFactory;
 use calendar::date::Date;
@@ -15,7 +16,7 @@ use weather::Weather;
 
 /// This function drives the simulation, after having parsed and built
 /// the Building, State and Peoeple.
-pub fn run(start: Date, end: Date, person: &dyn People, building: &Building, state: &mut BuildingState, weather: &dyn Weather, n: usize)->Result<(),String>{
+pub fn run(start: Date, end: Date, person: &dyn People, building: &Building, state: &mut SimulationState, weather: &dyn Weather, n: usize)->Result<(),String>{
     
     if start.is_equal(end) || start.is_later(end) {
         return Err(format!("Time period inconsistency... Start = {} | End = {}", start, end));
@@ -33,13 +34,9 @@ pub fn run(start: Date, end: Date, person: &dyn People, building: &Building, sta
 
     state.print_header();
 
-    // Simulate the whole simulation perod
+    // Simulate the whole simulation period
     for date in sim_period {    
-        
-        // Print results
-        state.print_values();
-        println!("==");
-        
+            
         // Get the current weather data
         let current_weather = weather.get_weather_data(date);
 
@@ -49,28 +46,12 @@ pub fn run(start: Date, end: Date, person: &dyn People, building: &Building, sta
             Err(e) => panic!(e)
         }
 
-        let dwelling_satisfaction = person.calculate_building_immediate_satisfaction(&building, &model, &state);
-
-        // Think about all this ONLY if the person attends
-        // the situation
-        if person.attend(date, dwelling_satisfaction) {
-            
-            // calculate current comfort, considering the future...
-            // So, clone the state.
-            let mut current_state_clone = state.clone();
-            // Perform the simulation
-            let (current_comfort, current_status) = person.forsee(date, building, &model, &mut current_state_clone);                   
-            // Reset the physics
-            current_state_clone.copy_physical_state_from(&state);
-
-            // Infer potential comfort
-            let (potential_comfort, potential_state) = person.infer_expected_outcomes(date, &building, &model, current_state_clone, current_comfort, &current_status);
-
-            // Behave (i.e. change the building state) if worth it.
-            person.behave_if_worth_it(date, current_comfort, potential_comfort, state, &potential_state);
-
-
-        }// end of if attend()
+        // Control the building or person, if needed        
+        if person.control(date, building, &model, state) {
+            println!("Person did something!");
+        }
+        println!("======")
+        
 
     }
     
@@ -92,131 +73,28 @@ pub fn run(start: Date, end: Date, person: &dyn People, building: &Building, sta
 mod testing{
     use super::*;
     
-    use geometry3d::polygon3d::Polygon3D;
-    use geometry3d::point3d::Point3D;
-    use geometry3d::loop3d::Loop3D;
-
-
-    use building_model::building::Building;
-    use building_model::substance::{SubstanceProperties};
-    use building_model::material::{MaterialProperties};
-    use building_model::boundary::Boundary;
-    use building_model::fenestration::*;
     
-    use building_model::heating_cooling::HeatingCoolingKind;
     
         
     use weather::synthetic_weather::SyntheticWeather;
-
     use schedule::constant::ScheduleConstant;
-
     use calendar::date::Date;
 
-    use people::person::Person;
-    use people::perceptions::Perception;
 
-
-    // A single space building with one operable window (it is opaque
-    // at the moment, but I don't think it matters) and a 1500W
-    // heater. The zone has only one Surface.
-    fn get_single_zone_building(state: &mut BuildingState)-> Building {
-
-        let mut building = Building::new("The Building".to_string()); 
-
-        // Add the space
-        let zone_volume = 40.;
-        let space_index = building.add_space("Some space".to_string());
-        building.set_space_volume(space_index,zone_volume).unwrap();
-
-        building.add_heating_cooling_to_space(state, space_index, HeatingCoolingKind::IdealHeaterCooler).unwrap();
-        building.set_space_max_heating_power(space_index, 1500.).unwrap();
-
-        // Add substance
-        let poly_index = building.add_substance("polyurethane".to_string());
-        building.set_substance_properties(poly_index, SubstanceProperties{
-            thermal_conductivity: 0.0252, // W/m.K            
-            specific_heat_capacity: 2400., // J/kg.K
-            density: 17.5, // kg/m3... reverse engineered from paper
-        }).unwrap();
-
-        // add material
-        let mat_index = building.add_material("20mm Poly".to_string());
-        building.set_material_properties(mat_index, MaterialProperties{
-            thickness: 20./1000.
-        }).unwrap();
-        building.set_material_substance(mat_index,poly_index).unwrap();
-
-        // Add construction
-        let c_index = building.add_construction("The construction".to_string());
-        building.add_material_to_construction(c_index, mat_index).unwrap();
-
-
-        // Create surface geometry
-        // Geometry
-        let mut the_loop = Loop3D::new();
-        let l = 4. as f64;
-        the_loop.push( Point3D::new(-l, -l, 0.)).unwrap();
-        the_loop.push( Point3D::new(l, -l, 0.)).unwrap();
-        the_loop.push( Point3D::new(l, l, 0.)).unwrap();
-        the_loop.push( Point3D::new(-l, l, 0.)).unwrap();
-        the_loop.close().unwrap();
-        
-        let mut p = Polygon3D::new(the_loop).unwrap();
-
-
-        let mut the_inner_loop = Loop3D::new();
-        let l = 1. as f64;
-        the_inner_loop.push( Point3D::new(-l, -l, 0.)).unwrap();
-        the_inner_loop.push( Point3D::new(l, -l, 0.)).unwrap();
-        the_inner_loop.push( Point3D::new(l, l, 0.)).unwrap();
-        the_inner_loop.push( Point3D::new(-l, l, 0.)).unwrap();
-        the_inner_loop.close().unwrap();
-        p.cut_hole(the_inner_loop.clone()).unwrap();
-
-        
-
-        // Add surface
-        let surface_index = building.add_surface("Surface".to_string());
-        building.set_surface_construction(surface_index,c_index).unwrap();
-        building.set_surface_polygon(surface_index, p).unwrap();
-        
-        building.set_surface_front_boundary(surface_index, Boundary::Space(space_index)).unwrap();
-
-        // Add window.        
-        let window_polygon = Polygon3D::new(the_inner_loop).unwrap();
-        let window_index = building.add_fenestration(state, "Window One".to_string(), OperationType::Binary, FenestrationType::Window);
-        building.set_fenestration_construction(window_index, c_index).unwrap();     
-        building.set_fenestration_polygon(window_index, window_polygon).unwrap();
-
-        
-
-        return building;
-
-    }
-
-    fn get_person()->Person {
-        let mut person = Person::new();
-        
-        person.set_proactivity(Box::new(ScheduleConstant::new(0.5))).unwrap();
-        person.set_busyness(Box::new(ScheduleConstant::new(0.5))).unwrap();
-        person.set_awareness_of_the_future(Box::new(ScheduleConstant::new(1.))).unwrap();
-
-        person.add_perception(0.2, Perception::ThermalSensationHot);
-        person.add_perception(0.2, Perception::ThermalSensationCold);
-
-        return person;
-    }
+    use simple_test_buildings::*;
+    use simple_test_people::*;
+    
 
 
     #[test]
     fn test_run(){
         
-        let mut state = BuildingState::new();        
+        let mut state = SimulationState::new();        
+        let person = get_person(&mut state);
         let building = get_single_zone_building(&mut state);
-        let person = get_person();
 
         let mut weather = SyntheticWeather::new();
-        weather.dry_bulb_temperature = Box::new(ScheduleConstant::new(23.));
+        weather.dry_bulb_temperature = Box::new(ScheduleConstant::new(-46.));
 
         let start = Date{
             day: 1,
@@ -225,9 +103,9 @@ mod testing{
         };
 
         let mut end = start.clone();
-        end.add_days(7);// simulate one week
+        end.add_hours(350.0);// simulate one week
 
-        let n = 1; // tsteps per hour
+        let n = 12; // tsteps per hour
 
         run(start, end, &person, &building, &mut state, &weather, n).unwrap();
 
