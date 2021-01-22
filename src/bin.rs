42e55242d18d688e76a3c7ed6096864810734860
German Molina <germolinal@gmail.com>
2021-01-22 20:04:41 +1300

    simulates something

--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,48 @@
+use std::env;
+
+use serde_json;        
+
+
+
+extern crate simple_lib;
+use simulation_state::simulation_state::SimulationState;
+use calendar::date::Date;
+use simple_test_buildings::*;
+use simple_test_people::*;
+use weather::epw_weather::EPWWeather;
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Error... Usage is: {} epw_file", args[0]);
+        return;
+    }
+    
+    let weather_file = args[1].clone();
+
+    
+    let mut state = SimulationState::new();        
+    let person = get_person(&mut state);
+    let building = get_single_zone_building_with_heater(&mut state, 20.0);
+
+    let weather = EPWWeather::from_file(weather_file);    
+
+    let start = Date{
+        day: 1,
+        month: 1,
+        hour: 0.0,
+    };
+
+    let mut end = start.clone();
+    end.add_hours(72.0);// simulate one week
+
+    let n = 12; // tsteps per hour
+
+    let results = simple_lib::run(start, end, &person, &building, &mut state, &weather, n).unwrap();
+
+    println!("{}",serde_json::to_string_pretty(&results).unwrap())
+    
+    
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,69 @@
+use std::collections::HashMap;
+
 use simulation_state::simulation_state::SimulationState;
 use building_model::building::Building;
 use communication_protocols::simulation_model::SimulationModel;
-//use controller::Controller;
-
 use calendar::date_factory::DateFactory;
 use calendar::date::Date;
-
-
 use people::people::People;
-
 use multiphysics_model::multiphysics_model::MultiphysicsModel;
-
 use weather::Weather;
+use simple_results::{SimulationResults, TimeStepResults};
 
 
 /// This function drives the simulation, after having parsed and built
 /// the Building, State and Peoeple.
-pub fn run(start: Date, end: Date, person: &dyn People, building: &Building, state: &mut SimulationState, weather: &dyn Weather, n: usize)->Result<(),String>{
+pub fn run(start: Date, end: Date, person: &dyn People, building: &Building, state: &mut SimulationState, weather: &dyn Weather, n: usize)->Result<SimulationResults,String>{
     
-    if start.is_equal(end) || start.is_later(end) {
+    
+    if start == end || start.is_later(end) {
         return Err(format!("Time period inconsistency... Start = {} | End = {}", start, end));
     }
-
+    
     let model = match MultiphysicsModel::new(&building, state, n){
         Ok(v)=>v,
         Err(e)=>return Err(e),
     };    
-
-
+    
+    
     // Calculate timestep and build the Simulation Period
     let dt = 60. * 60. / n as f64;
     let sim_period = DateFactory::new(start, end, dt);
-
-    state.print_header();
-
+        
+    // TODO: Calculate the capacity needed for the results
+    let mut results = SimulationResults::new();
+    
+    
     // Simulate the whole simulation period
     for date in sim_period {    
-            
+        
+        // initialize results struct
+        let mut step_results = TimeStepResults{
+            timestep_start : date,    
+            state_elements : state.elements().clone(),
+            weather : weather.get_weather_data(date),
+            controllers: HashMap::new() 
+        };
+        
         // Get the current weather data
         //let current_weather = weather.get_weather_data(date);
-
+        
         // Make the model march
         match model.march(date, weather, building, state ) {
             Ok(_)=>{},
             Err(e) => panic!(e)
         }
-
-        // Control the building or person, if needed        
-        if person.control(date, weather, building, &model, state) {
-            println!("Person did something!");
-        }
-        println!("======")
         
+        
+        // Control the building or person, if needed        
+        let person_result = person.control(date, weather, building, &model, state);
+        step_results.controllers.insert(format!("person"), person_result);
 
+        // push results
+        results.push(step_results);        
     }
     
-    Ok(())
+    Ok(results)
     
 }
 
@@ -73,7 +81,7 @@ pub fn run(start: Date, end: Date, person: &dyn People, building: &Building, sta
 mod testing{
     use super::*;
     
-    
+    use serde_json;
     
         
     use weather::synthetic_weather::SyntheticWeather;
@@ -94,7 +102,7 @@ mod testing{
         let building = get_single_zone_building(&mut state);
 
         let mut weather = SyntheticWeather::new();
-        weather.dry_bulb_temperature = Box::new(ScheduleConstant::new(222.));
+        weather.dry_bulb_temperature = Box::new(ScheduleConstant::new(42.));
 
         let start = Date{
             day: 1,
@@ -107,7 +115,9 @@ mod testing{
 
         let n = 12; // tsteps per hour
 
-        run(start, end, &person, &building, &mut state, &weather, n).unwrap();
+        let results = run(start, end, &person, &building, &mut state, &weather, n).unwrap();
+
+        println!("{}",serde_json::to_string_pretty(&results).unwrap())
 
 
     }
